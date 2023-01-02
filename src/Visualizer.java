4fb39c05faec85bc8603dd6be04c9f4ca6e2b328
FrederikvSvane <s224766@student.dtu.dk>
2023-01-02 19:09:30 +0100

    Centrering og farveændring af spillebrættets knapper

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.event.*;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.input.*;
 import javafx.scene.layout.GridPane;
@@ -34,17 +35,18 @@ public class Visualizer extends Application {
             for (int j = 0; j < width; j++) {
                 cells[i][j] = new Button();
                 board.add(cells[i][j], i, j);
+                if ((i + j) % 2 == 0) {
+                    cells[i][j].setStyle("-fx-background-color: BURLYWOOD; -fx-border-color: TAN");
+                } else {
+                    cells[i][j].setStyle("-fx-background-color: BLANCHEDALMOND; -fx-border-color: TAN");
+                }
                 cells[i][j].prefHeightProperty().bind(Bindings.divide(primaryStage.widthProperty(), 10.0));
                 cells[i][j].prefWidthProperty().bind(Bindings.divide(primaryStage.widthProperty(), 10.0));
             }
+            board.setAlignment(Pos.CENTER);
         }
 
-
-
-
-
         Scene scene = new Scene(board, 600, 600);
-        primaryStage.setFullScreen(true);
         primaryStage.setScene(scene);
         primaryStage.show();
 
