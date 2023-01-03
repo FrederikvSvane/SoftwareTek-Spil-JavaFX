b664c953fc97e75ae443256db6a63bc0b5b5c47e
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-01-03 12:22:15 +0100

    MenuStart

---
+++ src/Menu.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+
+
+public class Menu extends Application {
+
+    public static void Menu(String[]args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        @FXML Button button1;
+
+        @FXML
+        private void startGame() {
+            System.out.println("Start Game");
+        }
+
+    }
+
+}
+
